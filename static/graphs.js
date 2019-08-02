1ef6a0ce25bc7d65665d650749c568946b63de53
Joi <khrissianjoi@gmail.com>
2019-08-02 15:15:54 +0100

    girls names

--- static/graphs.js
+++ static/graphs.js
@@ -1,16 +1,37 @@
 
-createGraphs(transactionData);
-function createGraphs(dataSet) {
+createGraphsforTransactionData(transactionData);
+createGraphsforGirlNames(namesRank);
+
+function createGraphsforTransactionData(dataSet) {
     var ndx = crossfilter(dataSet);
     show_total_spend_per_state(ndx);
     show_total_spend_per_person(ndx);
     show_total_earnings_per_store(ndx);
+
     dc.renderAll();
+}
+
+function createGraphsforGirlNames(dataSet) {
+    var ndx = crossfilter(dataSet);
+    showGirlNamesRanking(ndx);
+    dc.renderAll();
+}
+
+function showGirlNamesRanking(ndx) {
+    var nameDim = ndx.dimension(dc.pluck("name"));
+    var countPerName = nameDim.group().reduceSum(dc.pluck("count"));
+    dc.rowChart("#girls-name")
+        .width(450)
+        .height(1500)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(nameDim)
+        .group(countPerName);
 }
 
 function show_total_spend_per_state(ndx) {
     var stateDim = ndx.dimension(dc.pluck("state"));
     var totalSpendPerState = stateDim.group().reduceSum(dc.pluck("spend"));
+
     dc.barChart("#per-state")
         .width(350)
         .height(250)
