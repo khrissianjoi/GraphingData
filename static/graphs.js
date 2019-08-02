2b611c10b4dfd0ed616fc2d0bfcaaa8e1950223e
Joi <khrissianjoi@gmail.com>
2019-08-02 12:39:24 +0100

    amount earned per store graph - rowchart

--- static/graphs.js
+++ static/graphs.js
@@ -4,18 +4,19 @@ function createGraphs(dataSet) {
     var ndx = crossfilter(dataSet);
     show_total_spend_per_state(ndx);
     show_total_spend_per_person(ndx);
+    show_total_earnings_per_store(ndx);
     dc.renderAll();
 }
 
 function show_total_spend_per_state(ndx) {
-    var state_dim = ndx.dimension(dc.pluck("state"));
-    var total_spend_per_state = state_dim.group().reduceSum(dc.pluck("spend"));
+    var stateDim = ndx.dimension(dc.pluck("state"));
+    var totalSpendPerState = stateDim.group().reduceSum(dc.pluck("spend"));
     dc.barChart("#per-state")
         .width(350)
         .height(250)
         .margins({top: 10, right: 50, bottom: 30, left: 50})
-        .dimension(state_dim)
-        .group(total_spend_per_state)
+        .dimension(stateDim)
+        .group(totalSpendPerState)
         .transitionDuration(500)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
@@ -25,8 +26,8 @@ function show_total_spend_per_state(ndx) {
 }
 
 function show_total_spend_per_person(ndx) {
-    var name_dim = ndx.dimension(dc.pluck("name"));
-    var total_spend_per_person = name_dim.group().reduceSum(dc.pluck("spend"));
+    var nameDim = ndx.dimension(dc.pluck("name"));
+    var totalSpendPerPerson = nameDim.group().reduceSum(dc.pluck("spend"));
     dc.lineChart("#per-person")
         .width(350)
         .height(250)
@@ -35,6 +36,16 @@ function show_total_spend_per_person(ndx) {
         .brushOn(false)
         .xAxisLabel('Name')
         .yAxisLabel('Spent')
-        .dimension(name_dim)
-        .group(total_spend_per_person);
+        .dimension(nameDim)
+        .group(totalSpendPerPerson);
+}
+
+function show_total_earnings_per_store(ndx) {
+    var storeDim = ndx.dimension(dc.pluck("store"));
+    var totalEarningsPerPerson = storeDim.group().reduceSum(dc.pluck("spend"));
+    dc.rowChart("#per-store")
+        .width(350)
+        .height(250)
+        .dimension(storeDim)
+        .group(totalEarningsPerPerson);
 }
